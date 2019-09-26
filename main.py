c77f3342c24c51d10c7f077522cdb9abe51b2389
Edilma <edilmaria@hotmail.com>
2019-09-26 11:32:52 -0400

    studio 6 finalized

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     watched = db.Column(db.Boolean)
+    ratings  = db.Column(db.String(5))
+
     
     # TODO: add a ratings column to the Movie table
 
@@ -38,14 +40,13 @@ def get_current_watchlist():
 def get_watched_movies():
     # For now, we are just pretending
     # returns the list of movies the user has already watched and crossed off
-    return [ "The Matrix", "The Princess Bride", "Buffy the Vampire Slayer" ]
+    return Movie.query.filter_by(watched=True).all()
 
 # Create a new route called rate_movie which handles a POST request on /rating-confirmation
 @app.route("/rating-confirmation", methods=['POST'])
 def rate_movie():
     movie_id = request.form['movie_id']
     rating = request.form['rating']
-
     movie = Movie.query.get(movie_id)
     if movie not in get_watched_movies():
         # the user tried to rate a movie that isn't in their list,
@@ -56,10 +57,9 @@ def rate_movie():
         return redirect("/?error=" + error)
 
     # if we didn't redirect by now, then all is well
-    
-    # TODO: make a persistent change to the model so that you STORE the rating in the database
-    # (Note: the next TODO is in templates/ratings.html)
-    
+    movie.ratings = rating
+    db.session.commit()
+
     return render_template('rating-confirmation.html', movie=movie, rating=rating)
 
 
